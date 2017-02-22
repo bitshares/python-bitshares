17d3a50ef982f12e301f44aefcc22e3beb743dbe
Fabian Schuh <Fabian@chainsquad.com>
2017-02-22 10:43:01 +0100

    [chains] updated

--- bitsharesapi/bitsharesnoderpc.py
+++ bitsharesapi/bitsharesnoderpc.py
@@ -89,4 +89,4 @@ class BitSharesNodeRPC(GrapheneWebsocketRPC):
         for k, v in known_chains.items():
             if v["chain_id"] == chain_id:
                 return v
-        log.critical("Connecting to unknown network!")
+        raise("Connecting to unknown network!")
