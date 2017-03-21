77d362244c9d5f61b0775855116b5f683e7e43ec
Fabian Schuh <Fabian@chainsquad.com>
2017-03-21 08:46:57 +0100

    version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except LookupError:
     ascii = codecs.lookup('ascii')
     codecs.register(lambda name, enc=ascii: {True: enc}.get(name == 'mbcs'))
 
-VERSION = '0.1.4'
+VERSION = '0.1.4a0'
 
 setup(
     name='bitshares',
