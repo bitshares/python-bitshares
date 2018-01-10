d501936ac092f155fe90043bfbaf4e8e7817edde
Fabian Schuh <Fabian@chainsquad.com>
2018-01-10 08:38:16 +0100

    [setup] dependency of graphenelib pushed by one release

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         'Topic :: Office/Business :: Financial',
     ],
     install_requires=[
-        "graphenelib>=0.5.5",
+        "graphenelib>=0.5.6",
         "websockets",
         "appdirs",
         "Events",
