118a55fa2378e916aa4abeb135c501adc1eee221
Chris Beaven <smileychris@gmail.com>
2018-01-20 23:32:56 +1300

    Comment about the magic `mocker` argument

--- tests/test_asset.py
+++ tests/test_asset.py
@@ -2,6 +2,8 @@ from bitshares.asset import Asset
 from bitshares import BitShares
 
 
+# Mocker comes from pytest-mock, providing an easy way to have patched objects
+# for the life of the test.
 def test_calls(mocker):
     asset = Asset("USD", lazy=True, bitshares_instance=BitShares(offline=True))
     method = mocker.patch.object(Asset, 'get_call_orders')
