d07a1490be60fd79a2a51857f409a25ebaea1c69
Fabian Schuh <Fabian@chainsquad.com>
2018-02-05 14:41:35 +0100

    dependency bump

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         'Topic :: Office/Business :: Financial',
     ],
     install_requires=[
-        "graphenelib>=0.5.8",
+        "graphenelib>=0.5.9",
         "websockets",
         "appdirs",
         "Events",
