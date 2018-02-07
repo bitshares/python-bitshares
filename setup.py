d46d526f4212cfdd06c398a4bdcffa0c7611a666
mikakoi <mika@codaone.fi>
2018-02-07 10:50:11 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except LookupError:
     ascii = codecs.lookup('ascii')
     codecs.register(lambda name, enc=ascii: {True: enc}.get(name == 'mbcs'))
 
-VERSION = '0.1.11'
+VERSION = '0.1.11.beta'
 
 setup(
     name='bitshares',
