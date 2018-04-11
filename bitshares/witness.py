3f01378203b7c0d163be05e5a7b07418c6182a79
Fabian Schuh <Fabian@chainsquad.com>
2018-04-11 13:27:44 +0200

    [witness] identify if witness is active

--- bitshares/witness.py
+++ bitshares/witness.py
@@ -28,11 +28,22 @@ class Witness(BlockchainObject):
             witness = self.bitshares.rpc.get_witness_by_account(account["id"])
         if not witness:
             raise WitnessDoesNotExistsException(self.identifier)
-        super(Witness, self).__init__(witness, bitshares_instance=self.bitshares)
+        super(Witness, self).__init__(
+            witness, bitshares_instance=self.bitshares)
 
     @property
     def account(self):
-        return Account(self["witness_account"], bitshares_instance=self.bitshares)
+        return Account(
+            self["witness_account"], bitshares_instance=self.bitshares)
+
+    @property
+    def is_active(self):
+        account = Account(
+            "witness-account",
+            bitshares_instance=self.bitshares)
+        return self.account["id"] in [
+            x[0] for x in account["active"]["account_auths"]
+        ]
 
 
 class Witnesses(list):
@@ -46,7 +57,7 @@ class Witnesses(list):
     def __init__(self, only_active=False, bitshares_instance=None):
         self.bitshares = bitshares_instance or shared_bitshares_instance()
         total_witnesses = self.bitshares.rpc.get_witness_count()
-        ws = ["1.6.{:d}".format(i+1) for i in range(total_witnesses-1)]
+        ws = ["1.6.{:d}".format(i + 1) for i in range(total_witnesses - 1)]
         witnesses = [
             Witness(x, lazy=True, bitshares_instance=self.bitshares)
             for x in ws
