a8d2f810e92bf0a997417764c1bc25de5ae9ebc5
Fabian Schuh <Fabian@chainsquad.com>
2018-04-16 11:35:56 +0200

    [price] different json()-ification of price to represent base with smaller numbers

--- bitshares/price.py
+++ bitshares/price.py
@@ -221,10 +221,26 @@ class Price(dict):
         return self
 
     def json(self):
+        """
         return {
             "base": self["base"].json(),
             "quote": self["quote"].json()
         }
+        """
+        quote = self["quote"]
+        base = self["base"]
+        frac = Fraction(
+            int(quote) / int(base)
+        ).limit_denominator(10 ** base["asset"]["precision"])
+        return {
+            "base": {
+                "amount": int(frac.denominator),
+                "asset_id": base["asset"]["id"]
+            },
+            "quote": {
+                "amount": int(frac.numerator),
+                "asset_id": quote["asset"]["id"]
+            }}
 
     def __repr__(self):
         return "{price:.{precision}f} {base}/{quote}".format(
