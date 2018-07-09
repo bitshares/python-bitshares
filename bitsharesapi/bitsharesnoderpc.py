7ac52a56e66d9140e4968111e8b82fd5bd63b072
Fabian Schuh <Fabian@chainsquad.com>
2018-07-09 14:12:34 +0200

    New way of dealing with GrapheneAPI

--- bitsharesapi/bitsharesnoderpc.py
+++ bitsharesapi/bitsharesnoderpc.py
@@ -1,73 +1,37 @@
 import re
-from grapheneapi.graphenewsrpc import GrapheneWebsocketRPC
-from grapheneapi.graphenehttprpc import GrapheneHTTPRPC
-from bitsharesbase.chains import known_chains
-from . import exceptions
-import logging
-log = logging.getLogger(__name__)
+from grapheneapi.api import Api as Original_Api
+from .  import exceptions
 
 
-class BitSharesNodeRPC(GrapheneWebsocketRPC, GrapheneHTTPRPC):
-
-    def __init__(self,
-                 urls,
-                 user="",
-                 password="",
-                 **kwargs):
-        if isinstance(urls, str):
-            self._urls = re.split(r",|;", urls)
-        elif isinstance(urls, (list, tuple, set)):
-            self._urls = urls
+class Api(Original_Api):
+    def post_process_exception(self, e):
+        msg = exceptions.decodeRPCErrorMsg(e).strip()
+        if msg == "missing required active authority":
+            raise exceptions.MissingRequiredActiveAuthority
+        elif re.match("current_account_itr == acnt_indx.indices().get<by_name>().end()", msg):
+            raise exceptions.AccountCouldntBeFoundException(msg)
+        elif re.match("Assert Exception: is_valid_name( name )", msg):
+            raise exceptions.InvalidAccountNameException(msg)
+        elif re.match("^no method with name.*", msg):
+            raise exceptions.NoMethodWithName(msg)
+        elif msg:
+            raise exceptions.UnhandledRPCError(msg)
         else:
-            raise exceptions.InvalidEndpointUrl
-        self.rpc.__init__(self, self._urls, **kwargs)
-        self.chain_params = self.get_network()
-
-    def next(self):
-        """ Iterate over to the next API node an reconnect
-        """
-        self.rpc.next()
-
-    @property
-    def rpc(self):
-        first_url = self._urls[0]
-
-        if first_url[:2] == "ws":
-            # Websocket connection
-            return GrapheneWebsocketRPC
-        else:
-            # RPC/HTTP connection
-            return GrapheneHTTPRPC
-
-    def rpcexec(self, payload):
-        """ Execute a call by sending the payload.
-            It makes use of the GrapheneRPC library.
-            In here, we mostly deal with BitShares specific error handling
-
-            :param json payload: Payload data
-            :raises ValueError: if the server does not respond in proper JSON
-                                format
-            :raises RPCError: if the server returns an error
-        """
-        try:
-            # Forward call to GrapheneWebsocketRPC and catch+evaluate errors
-            return self.rpc.rpcexec(self, payload)
-        except exceptions.RPCError as e:
-            msg = exceptions.decodeRPCErrorMsg(e).strip()
-            if msg == "missing required active authority":
-                raise exceptions.MissingRequiredActiveAuthority
-            elif re.match("current_account_itr == acnt_indx.indices().get<by_name>().end()", msg):
-                raise exceptions.AccountCouldntBeFoundException(msg)
-            elif re.match("Assert Exception: is_valid_name( name )", msg):
-                raise exceptions.InvalidAccountNameException(msg)
-            elif re.match("^no method with name.*", msg):
-                raise exceptions.NoMethodWithName(msg)
-            elif msg:
-                raise exceptions.UnhandledRPCError(msg)
-            else:
-                raise e
-        except Exception as e:
             raise e
+
+
+class BitSharesNodeRPC(Api):
+
+    def get_network(self):
+        """ Identify the connected network. This call returns a
+            dictionary with keys chain_id, core_symbol and prefix
+        """
+        props = self.get_chain_properties()
+        chain_id = props["chain_id"]
+        for k, v in known_chains.items():
+            if v["chain_id"] == chain_id:
+                return v
+        raise Exception("Connecting to unknown network!")
 
     def get_account(self, name, **kwargs):
         """ Get full account details from account name or id
@@ -95,20 +59,3 @@ class BitSharesNodeRPC(GrapheneWebsocketRPC, GrapheneHTTPRPC):
             :param str o: Full object id
         """
         return self.get_objects([o], **kwargs)[0]
-
-    def get_network(self):
-        """ Identify the connected network. This call returns a
-            dictionary with keys chain_id, core_symbol and prefix
-        """
-        props = self.get_chain_properties()
-        chain_id = props["chain_id"]
-        for k, v in known_chains.items():
-            if v["chain_id"] == chain_id:
-                return v
-        raise Exception("Connecting to unknown network!")
-
-    def __getattr__(self, name):
-        """ Map all methods to RPC calls and pass through the arguments.
-            It makes use of the GrapheneRPC library.
-        """
-        return self.rpc.__getattr__(self, name)
