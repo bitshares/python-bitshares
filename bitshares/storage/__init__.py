f5e7ede856bc3cb7a0d4d9a76f79a2c2b73c6904
Fabian Schuh <Fabian@chainsquad.com>
2018-08-06 13:56:28 +0200

    call to obtain only the store

--- bitshares/storage/__init__.py
+++ bitshares/storage/__init__.py
@@ -1,37 +1,18 @@
-#from .sqlite import SqliteKey as K, SqliteConfiguration as C
-from .base import DefaultConfigurationStore
-from .base import DefaultEncryptedKeyStore
-from .base import MasterPassword
-from .base import InRamKeyStore
-from . import sqlite
+from .base import (
+    InRamConfigurationStore,
+    InRamPlainKeyStore,
+    InRamEncryptedKeyStore,
+    SqliteConfigurationStore,
+    SqlitePlainKeyStore,
+    SqliteEncryptedKeyStore
+)
 
 __all__ = [
     "base",
-    "sqlite"
+    "sqlite",
+    "ram"
 ]
-Configuration = DefaultConfigurationStore
-EncryptedKey = DefaultEncryptedKeyStore
-Key = InRamKeyStore
 
-# Create keyStorage
-configStorage = Configuration()
 
-masterPassword = MasterPassword(
-    storage=configStorage
-)
-
-keyStorage = Key(
-    storage=configStorage
-)
-
-# TODO
-"""
-# Create Tables if database is brand new
-if not configStorage.exists():
-    configStorage.create()
-
-newKeyStorage = False
-if not keyStorage.exists():
-    newKeyStorage = True
-    keyStorage.create()
-"""
+def get_default_config_store():
+    return SqliteConfigurationStore()
