352074d46dd2b585080a64b0a87f794f3cfe4483
Fabian Schuh <mail@xeroc.org>
2018-10-11 12:34:18 +0200

    fix setup.py URL

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     version=VERSION,
     description='Python library for bitshares',
     long_description=open('README.md').read(),
-    download_url='{}/tarball/{}'.format(VERSION, URL),
+    download_url='{}/tarball/{}'.format(URL, VERSION),
     author='Fabian Schuh',
     author_email='Fabian@chainsquad.com',
     maintainer='Fabian Schuh',
