63a40308061695cb9a7c6e8382feb5a38919ec55
Fabian Schuh <Fabian@chainsquad.com>
2018-12-20 11:11:22 +0100

    Import Object from graphenecommon

--- bitshares/blockchainobject.py
+++ bitshares/blockchainobject.py
@@ -2,10 +2,16 @@
 from .instance import BlockchainInstance
 from graphenecommon.blockchainobject import (
     BlockchainObject as GrapheneBlockchainObject,
+    Object as GrapheneObject,
     ObjectCache,
 )
 
 
 @BlockchainInstance.inject
 class BlockchainObject(GrapheneBlockchainObject):
+    pass
+
+
+@BlockchainInstance.inject
+class Object(GrapheneObject):
     pass
