61712dbd67e47597d5b7f988d1e64958a4a16e41
Fabian Schuh <mail@xeroc.org>
2019-02-01 09:06:58 +0100

    Ensure we do have a default expiration time for new orders

--- bitshares/storage.py
+++ bitshares/storage.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from graphenestorage import (
     InRamConfigurationStore,
     InRamEncryptedKeyStore,
@@ -10,8 +11,8 @@ from graphenestorage import (
 
 
 url = "wss://node.bitshares.eu"
-InRamConfigurationStore.setdefault("node", url)
 SqliteConfigurationStore.setdefault("node", url)
+SqliteConfigurationStore.setdefault("order-expiration", 356 * 24 * 60 * 60)
 
 
 def get_default_config_store(*args, **kwargs):
