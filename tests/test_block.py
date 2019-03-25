e3a7cb1d848390b29f02c584b0ed5a52df69d625
Fabian Schuh <mail@xeroc.org>
2019-03-25 16:31:57 +0100

    Fix test_block.py unittest to work with caching

--- tests/test_block.py
+++ tests/test_block.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import unittest
 from pprint import pprint
 from bitshares import BitShares
@@ -8,16 +9,15 @@ from .fixtures import fixture_data
 
 
 class Testcases(unittest.TestCase):
-
     def setUp(self):
         fixture_data()
 
     def test_block(self):
-        block = Block(1)
+        block = Block(1, use_cache=True)
         self.assertEqual(block["previous"], "0000000000000000000000000000000000000000")
-        self.assertEqual(block.time(), parse_time('2015-10-13T14:12:24'))
+        self.assertEqual(block.time(), parse_time("2015-10-13T14:12:24"))
 
     def test_blockheader(self):
-        header = BlockHeader(1)
+        header = BlockHeader(1, use_cache=True)
         self.assertEqual(header["previous"], "0000000000000000000000000000000000000000")
-        self.assertEqual(header.time(), parse_time('2015-10-13T14:12:24'))
+        self.assertEqual(header.time(), parse_time("2015-10-13T14:12:24"))
