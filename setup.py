e72b6e5ea28f6f67a5b9660588a96f757428a886
Fabian Schuh <mail@xeroc.org>
2019-04-03 14:51:55 +0200

    version bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except LookupError:
     ascii = codecs.lookup("ascii")
     codecs.register(lambda name, enc=ascii: {True: enc}.get(name == "mbcs"))
 
-VERSION = "0.3.0"
+VERSION = "0.3.1"
 URL = "https://github.com/bitshares/python-bitshares"
 
 setup(
