a3d7aa27ce3b204e5fe56b3926bb26dce2cd51ce
Fabian Schuh <mail@xeroc.org>
2019-08-20 14:39:02 +0200

    semverioner release updates

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+# Changelog
+Note: version releases in the 0.x.y range may introduce breaking changes.
+
+## 0.3.3
+
+- patch: New docs
+- patch: Released with semversioner
+- patch: pyup updates
+
+## 0.3.2
+
+- patch: Status Quo
+
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except LookupError:
     ascii = codecs.lookup("ascii")
     codecs.register(lambda name, enc=ascii: {True: enc}.get(name == "mbcs"))
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 URL = "https://github.com/bitshares/python-bitshares"
 
 setup(
