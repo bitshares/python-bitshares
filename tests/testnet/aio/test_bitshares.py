478ca3d0dd337210e97f22896e63359b9e2719be
Vladimir Kamarzin <vvk@vvk.pp.ru>
2019-11-08 20:10:05 +0500

    Add more tests for aio BitShares

--- tests/testnet/aio/test_bitshares.py
+++ tests/testnet/aio/test_bitshares.py
@@ -3,10 +3,29 @@ import asyncio
 import pytest
 import logging
 
+from datetime import datetime
+from bitshares.aio.asset import Asset
+from bitshares.aio.amount import Amount
+from bitshares.aio.price import Price
 from bitshares.aio.proposal import Proposals
+from bitshares.aio.worker import Workers
 
 log = logging.getLogger("grapheneapi")
 log.setLevel(logging.DEBUG)
+
+
+@pytest.fixture(scope="module")
+async def testworker(bitshares, default_account):
+    amount = await Amount("1000 TEST")
+    end = datetime(2099, 1, 1)
+    await bitshares.create_worker("test", amount, end, account=default_account)
+
+
+@pytest.fixture(scope="module")
+async def bitasset(create_asset, default_account):
+    await create_asset("CNY", 5, is_bitasset=True)
+    asset = await Asset("CNY")
+    await asset.update_feed_producers([default_account])
 
 
 @pytest.mark.asyncio
@@ -116,3 +135,98 @@ async def test_disapprove_proposal(bitshares, default_account, unused_account):
     await bitshares.approveproposal(proposals[0]["id"], account=account)
     # Revoke vote
     await bitshares.disapproveproposal(proposals[0]["id"], account=account)
+
+
+@pytest.mark.asyncio
+async def test_approve_disapprove_worker(bitshares, testworker, default_account):
+    workers = await Workers(default_account)
+    worker = workers[0]["id"]
+    await bitshares.approveworker(worker)
+    await bitshares.disapproveworker(worker)
+
+
+@pytest.mark.asyncio
+async def test_set_unset_proxy(bitshares, default_account):
+    await bitshares.set_proxy("init1", account=default_account)
+    await bitshares.unset_proxy()
+
+
+@pytest.mark.skip(reason="cancel() tested indirectly in test_market.py")
+@pytest.mark.asyncio
+async def test_cancel():
+    pass
+
+
+@pytest.mark.skip(reason="need to provide a way to make non-empty vesting balance")
+@pytest.mark.asyncio
+async def test_vesting_balance_withdraw(bitshares, default_account):
+    balances = await bitshares.rpc.get_vesting_balances(default_account)
+    await bitshares.vesting_balance_withdraw(balances[0]["id"], account=default_account)
+
+
+@pytest.mark.asyncio
+async def test_publish_price_feed(bitshares, default_account, bitasset):
+    price = await Price("1.0 CNY/TEST")
+    await bitshares.publish_price_feed("CNY", price, account=default_account)
+
+
+@pytest.mark.asyncio
+async def test_update_cer(bitshares, default_account):
+    pass
+
+
+@pytest.mark.asyncio
+async def test_update_witness(bitshares, default_account):
+    pass
+
+
+@pytest.mark.asyncio
+async def test_reserve(bitshares, default_account):
+    pass
+
+
+@pytest.mark.asyncio
+async def test_create_asset(bitshares, default_account, bitasset):
+    asset = await Asset("CNY")
+    assert asset.is_bitasset
+
+
+@pytest.mark.asyncio
+async def test_create_worker(testworker, default_account):
+    w = await Workers(default_account)
+    assert len(w) > 0
+
+
+@pytest.mark.asyncio
+async def test_fund_fee_pool(bitshares, default_account):
+    pass
+
+
+@pytest.mark.asyncio
+async def test_create_committee_member(bitshares, default_account):
+    pass
+
+
+@pytest.mark.asyncio
+async def test_account_whitelist(bitshares, default_account):
+    pass
+
+
+@pytest.mark.asyncio
+async def test_bid_collateral(bitshares, default_account):
+    pass
+
+
+@pytest.mark.asyncio
+async def test_asset_settle(bitshares, default_account):
+    pass
+
+
+@pytest.mark.asyncio
+async def test_htlc_create(bitshares, default_account):
+    pass
+
+
+@pytest.mark.asyncio
+async def test_htlc_redeem(bitshares, default_account):
+    pass
