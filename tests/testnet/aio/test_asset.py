bitshares/python-bitshares

commit bbfefab39418ff3106c56265fd66cfc186d0b51e

cast amount to int to avoid endless loop

bitshares/asset.py
@@ -581,7 +581,7 @@ class Asset(GrapheneAsset):
                 },  # Will be filled in automatically
                 "issuer": account["id"],  # the Issuer account
                 "asset_to_issue": {
-                    "amount": int(amount * 10 ** self["precision"]),
+                    "amount": int(int(amount) * 10 ** self["precision"]),
                     "asset_id": self["id"],
                 },
                 "issue_to_account": to["id"],

commit 561a3f4cc789e5a16ec2467f1654a2f714d68bac

Fix improper type cast in Asset.issue()
Regression introduced in bbfefab39418ff3106c56265fd66cfc186d0b51e.
Casting amount to int leads to effectively zeroing amounts < 1 (e.g.
0.01 -> 0), causing a node error:
grapheneapi.exceptions.RPCError: asset_to_issue.amount.value > 0:

bitshares/aio/asset.py
@@ -504,7 +504,7 @@ class Asset(GrapheneAsset, SyncAsset):
                 },  # Will be filled in automatically
                 "issuer": account["id"],  # the Issuer account
                 "asset_to_issue": {
-                    "amount": int(amount * 10 ** self["precision"]),
+                    "amount": int(float(amount) * 10 ** self["precision"]),
                     "asset_id": self["id"],
                 },
                 "issue_to_account": to["id"],

bitshares/asset.py
@@ -581,7 +581,7 @@ class Asset(GrapheneAsset):
                 },  # Will be filled in automatically
                 "issuer": account["id"],  # the Issuer account
                 "asset_to_issue": {
-                    "amount": int(int(amount) * 10 ** self["precision"]),
+                    "amount": int(float(amount) * 10 ** self["precision"]),
                     "asset_id": self["id"],
                 },
                 "issue_to_account": to["id"],

tests/testnet/aio/test_asset.py
@@ -197,4 +197,9 @@ async def test_change_issuer(testasset):
 
 @pytest.mark.asyncio
 async def test_issue(testasset):
+    # Normal invokation
     await testasset.issue(100, "init1")
+    # Amount as str
+    await testasset.issue("1", "init1")
+    # Amount < 1
+    await testasset.issue(0.01, "init1")