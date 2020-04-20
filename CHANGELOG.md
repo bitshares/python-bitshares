dddb3a9f7aae1f0eddd417334d8b6969494f32ff
Fabian Schuh <mail@xeroc.org>
2020-04-20 14:04:13 +0200

    semversioner release updates

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 # Changelog
 Note: version releases in the 0.x.y range may introduce breaking changes.
 
+## 0.5.1
+
+- patch: Add aio packages to setup.py
+
 ## 0.5.0
 
 - minor: Add asyncio support
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except LookupError:
     ascii = codecs.lookup("ascii")
     codecs.register(lambda name, enc=ascii: {True: enc}.get(name == "mbcs"))
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 URL = "https://github.com/bitshares/python-bitshares"
 
 setup(
