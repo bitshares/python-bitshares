5bce0da10de49f85a169bef71d4a839fcdfb80a1
Eric Frias <efrias@syncad.com>
2020-08-03 19:40:39 -0400

    Add support for the new ticket_create_operation and ticket_update_operation

--- bitsharesbase/operations.py
+++ bitsharesbase/operations.py
@@ -1042,5 +1042,63 @@ class Assert(GrapheneObject):
                 )
             )
 
+ticket_type_strings = ['liquid', 'lock_180_days', 'lock_360_days', 'lock_720_days', 'lock_forever']
+
+class Ticket_create_operation(GrapheneObject):
+    def __init__(self, *args, **kwargs):
+        if isArgsThisClass(self, args):
+            self.data = args[0].data
+        else:
+            if len(args) == 1 and len(kwargs) == 0:
+                kwargs = args[0]
+
+            if isinstance(kwargs["target_type"], int):
+                target_type = Varint32(kwargs["target_type"])
+            else:
+                target_type = Varint32(ticket_type_strings.index(kwargs["target_type"]))
+
+            super().__init__(
+                OrderedDict(
+                    [
+                        ("fee", Asset(kwargs["fee"])),
+                        ("account", ObjectId(kwargs["account"], "account")),
+                        ("target_type", target_type),
+                        ("amount", Asset(kwargs["amount"])),
+                        ("extensions", Set([])),
+                    ]
+                )
+            )
+
+class Ticket_update_operation(GrapheneObject):
+    def __init__(self, *args, **kwargs):
+        if isArgsThisClass(self, args):
+            self.data = args[0].data
+        else:
+            if len(args) == 1 and len(kwargs) == 0:
+                kwargs = args[0]
+
+            if isinstance(kwargs["target_type"], int):
+                target_type = Varint32(kwargs["target_type"])
+            else:
+                target_type = Varint32(ticket_type_strings.index(kwargs["target_type"]))
+
+            if kwargs.get("amount_for_new_target"):
+                amount_for_new_target = Optional(Asset(kwargs["amount_for_new_target"]))
+            else:
+                amount_for_new_target = Optional(None)
+
+            super().__init__(
+                OrderedDict(
+                    [
+                        ("fee", Asset(kwargs["fee"])),
+                        ("ticket", ObjectId(kwargs["ticket"], "ticket")),
+                        ("account", ObjectId(kwargs["account"], "account")),
+                        ("target_type", target_type),
+                        ("amount_for_new_target", amount_for_new_target),
+                        ("extensions", Set([])),
+                    ]
+                )
+            )
+
 
 fill_classmaps()
