0defdc56d70710df89c4a21e02474738f7848234
Eric Frias <efrias@syncad.com>
2020-08-03 19:40:39 -0400

    Add support for the new ticket_create_operation and ticket_update_operation

--- bitsharesbase/objecttypes.py
+++ bitsharesbase/objecttypes.py
@@ -18,4 +18,6 @@ object_type["vesting_balance"] = 13
 object_type["worker"] = 14
 object_type["balance"] = 15
 object_type["htlc"] = 16
-object_type["OBJECT_TYPE_COUNT"] = 16
+object_type["custom_authority"] = 17
+object_type["ticket"] = 18
+object_type["OBJECT_TYPE_COUNT"] = 18
