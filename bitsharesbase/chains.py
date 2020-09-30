ed3bd1b1042aa997b90c95c836af4d2529ff91f4
bangzi1001 <36911788+bangzi1001@users.noreply.github.com>
2020-09-30 11:13:35 +0800

    Add NBS Chain ID

--- bitsharesbase/chains.py
+++ bitsharesbase/chains.py
@@ -15,4 +15,9 @@ known_chains = {
         "core_symbol": "TEST",
         "prefix": "TEST",
     },
+    "NBS": {
+        "chain_id": "cd931cb96d657ff0ef0226f7ae9d25175b3cc96a84490a674ed36170830324e7",
+        "core_symbol": "NBS",
+        "prefix": "NBS",
+    },
 }
